13d1765db79134a4478dda5a1dfb939cacb5d517
ahmadm712 <ahmadmuji2808@gmail.com>
2021-10-06 01:08:46 +0700

    update

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./footer.css";
 function Footer() {
-  return <div className="footer">Made with 🔥 by Overreacted</div>;
+  return <div className="footer">© 2021 Ahmad Muji | All rights Reserved.</div>;
 }
 
 export default Footer;
