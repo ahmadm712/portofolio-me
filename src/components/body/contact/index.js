c97c723c7aad6aaef838ecf1eadfeaca16741420
ahmadm712 <ahmadmuji2808@gmail.com>
2021-10-06 02:01:07 +0700

    update

--- src/components/body/contact/index.js
+++ src/components/body/contact/index.js
@@ -13,7 +13,7 @@ function Contact() {
           <SocialContact />
         </div>
         <div className="download">
-          <a download href={require("../../../assets/CV new.pdf").default}>
+          <a download href={require("../../../assets/CV Ahmad.pdf").default}>
             <i class="fi-rr-cloud-download download-icon" />
             Download Resume
           </a>
