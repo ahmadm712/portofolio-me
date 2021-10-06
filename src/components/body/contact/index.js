a479e125db24978f6c1762be88548ffb9ca2e9b1
ahmadm712 <ahmadmuji2808@gmail.com>
2021-10-06 16:02:05 +0700

    update and add email

--- src/components/body/contact/index.js
+++ src/components/body/contact/index.js
@@ -3,27 +3,6 @@ import "./contact.css";
 import SocialContactMobile from "../../common/social-contact-mobile/index";
 import Separator from "../../common/separator/index";
 function Contact() {
-  function encode(data) {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": event.target.getAttribute("name"),
-        ...name,
-      }),
-    })
-      
-      .catch((error) => alert(error));
-  };
   return (
     <div className="contact">
       <Separator />
@@ -40,19 +19,7 @@ function Contact() {
           </a>
         </div>
       </div>
-      <form
-        data-netlify="true"
-        name="pizzaOrder"
-        method="post"
-        onSubmit={handleSubmit}
-      >
-        <input type="hidden" name="form-name" value="pizzaOrder" />
-        <label>
-          What order did the pizza give to the pineapple?
-          <input name="order" type="text"  />
-        </label>
-        <input type="submit" />
-      </form>
+     
     </div>
   );
 }
