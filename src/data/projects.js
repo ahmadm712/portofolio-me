be7737c1ed1aa64c762a3226869457a6a92a5b5e
ahmadm712 <ahmadmuji2808@gmail.com>
2021-11-04 16:04:40 +0700

    add update on project detail

--- src/data/projects.js
+++ src/data/projects.js
@@ -27,12 +27,14 @@ export const ProjectData = [
     title: "COVINDO",
     about:
       "Covindo adalah sebuah platform layanan penyedia informasi bagi masyarakat Indonesia untuk mempersiapkan diri pada situasi darurat selama pandemi Covid-19. Dengan informasi faktual, serta kemudahan akses darimanapun menjadikan sebuah platform yang dapat diandalkan masyarakat Indonesia.",
-    demoAkun: "Web ini Dibuat untuk menyelesaikan Tugas Kelompok Stage 1 WEB DEVELOPMENT, Study Independen Bersama Kelompok 10",
+    demoAkun:
+      "Web ini Dibuat untuk menyelesaikan Tugas Kelompok Stage 1 WEB DEVELOPMENT, Study Independen Bersama Kelompok 10 ,This project is part of the Product Development and Coding training program (Skilvul #Tech4Impact) implemented by Skilvul (https://skilvul.com/), for Kampus Merdeka program held by Kementerian Pendidikan, Kebudayaan, Riset, dan Teknologi Republik Indonesia. ASEAN Youth Forum is the Challenge Partner. I am not working for nor contracted professionally by ASEAN Youth Forum.",
     tags: ["Restful API", "HTML", "CSS", "Javascript", "Bootstrap", "Netlify"],
 
     demo: "https://covindo10.netlify.app/",
 
-    github: "https://github.com/Muhammad-Ikhwan-Fathulloh/Skilvul-COVINDO-Kelompok-10",
+    github:
+      "https://github.com/Muhammad-Ikhwan-Fathulloh/Skilvul-COVINDO-Kelompok-10",
 
     image: "https://i.ibb.co/c186MCy/COVINDO-SS.png",
   },
