ed2544845cbe37421bd228e89a649a60f2a01ca5
ahmadm712 <ahmadmuji2808@gmail.com>
2021-12-09 23:29:01 +0700

    update work

--- src/data/work.js
+++ src/data/work.js
@@ -1,5 +1,13 @@
 export const WorkData = [
   {
+    company: "PT Cipta lestari Esatama",
+    designation: "Junior Software Engineer (Magang)",
+    dateJoining: "09-Nov-2021",
+    dateEnd: "09-Feb-2022",
+    companyLogo: "https://media-exp1.licdn.com/dms/image/C560BAQHw860qzyk0VQ/company-logo_200_200/0/1543518524710?e=1646870400&v=beta&t=cvxjr5yq2XNVs9kiegg_5pQFqoCDVKpvyHGwihLfOYU",
+    work: "Merancang dan Mengembangkan Sistem Web/App, Menerjemahkan Kebutuhan bisnis kedalam desain app/web.",
+  },
+  {
     company: "Skilvul",
     designation: "Study Independent (Kampus Merdeka)",
     dateJoining: "28-Aug-2021",
@@ -16,4 +24,5 @@ export const WorkData = [
     companyLogo: "https://i.ibb.co/4sPp8R7/sagita.jpg",
     work: "Membantu pengecekan Perangkat Komputer  mulai dari Monitor, CPU,hingga Printer Yang bermasalah, Membantu Menyelesaikan Pekerjaan Teknisi.",
   },
+  
 ];
